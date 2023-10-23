f9f27463994f2a56ac5c21e3bad8b85d5a0a89bd
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-23 17:27:10 +0400

    поменял название аргумента

--- places/admin.py
+++ places/admin.py
@@ -8,8 +8,8 @@ class PicsInline(SortableInlineAdminMixin, admin.TabularInline):
     model = PlaceImage
     readonly_fields = ["show_photo_preview"]
 
-    def show_photo_preview(self, obj):
-        return obj.show_photo_preview
+    def show_photo_preview(self, image):
+        return image.show_photo_preview
 
     show_photo_preview.short_description = 'Photo Preview'
     show_photo_preview.allow_tags = True
@@ -28,8 +28,8 @@ class PicAdmin(admin.ModelAdmin):
     raw_id_fields = ['place']
     readonly_fields = ["show_photo_preview"]
 
-    def show_photo_preview(self, obj):
-        return obj.show_photo_preview
+    def show_photo_preview(self, image):
+        return image.show_photo_preview
 
     show_photo_preview.short_description = 'Photo Preview'
     show_photo_preview.allow_tags = True
